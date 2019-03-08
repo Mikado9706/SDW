1baf2d0f1e73faecdcaf13da8c4601fc4ff6a802
Nicolas <nicolasbeck.dev@gmail.com>
2019-03-08 16:01:32 +0100

    adding fucntion behind etc button

--- public/js/custom.js
+++ public/js/custom.js
@@ -9,4 +9,20 @@ $(function() {
         $('.sidenav').sidenav('open');
     });
 
+    $("form[name='search'] .etc").click(function() {
+        if($(this).hasClass('active')){
+            $(this).removeClass('active');
+            $('.notSearch').each(function(){
+                $(this).addClass('disabled');
+            });
+
+        }else{
+            $(this).addClass('active');
+
+            $('.notSearch').each(function(){
+                $(this).removeClass('disabled');
+            });
+        }
+    });
+
 });
